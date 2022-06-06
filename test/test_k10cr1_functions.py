6352dbef76deb78c00a1923d1bf8570f2b57ec2e
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2022-06-06 11:16:41 +0900

    add some assertions

--- test/test_k10cr1_functions.py
+++ test/test_k10cr1_functions.py
@@ -2,13 +2,23 @@
 # -*- coding: utf-8 -*-
 """Tests for functions in k10cr1.py (dth, btd)"""
 
-from k10cr1 import dth
-from k10cr1 import btd
+from K10CR1.k10cr1 import dth
+from K10CR1.k10cr1 import btd
 
 
 class Test_dth:
-    pass
-
+    assert dth(4, 1) == '04'
+    assert dth(4, 4) == '04000000'
+    assert dth(4, 6) == '040000000000'
+    assert dth(-4, 4) == 'fcffffff'
+    assert dth(-4, 1) == 'fc'
+    assert dth(14, 4) == '0e000000'
+    assert dth(14, 1) == '0e'
+    assert dth(254, 4) == 'fe000000'
+    assert dth(255, 4) == 'ff000000'
+    assert dth(256, 4) == '00010000'
+    assert dth(-256, 4) == '00ffffff'
+    assert dth(-255, 4) == '01ffffff'
 
 class Test_btd:
     pass
