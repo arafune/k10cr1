13147427e29e2aea0000dfff20b69bcacaed9eaa
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2022-06-06 10:04:43 +0900

    add test_k10cr1_functionspy for testing and refactoring

---
+++ test/test_k10cr1_functions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Tests for functions in k10cr1.py (dth, btd)"""
+
+from k10cr1 import dth
+from k10cr1 import btd
+
+
+class Test_dth:
+    pass
+
+
+class Test_btd:
+    pass
